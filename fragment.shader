4af21e37b80f69ff8fd69ab21b759aa96e25a189
khaelar <midavmidav2000@gmail.com>
2024-10-08 15:15:56 +0300

    add solid color shader

---
+++ fragment.shader
@@ -0,0 +1,8 @@
+out vec4 color;
+
+uniform vec4 theColor;
+
+void main(){
+    // color = vec4(0.2, 0.7, 0.3, 1.0);
+    color = theColor;
+}
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import glfw
 from OpenGL.GL import *
+from pathlib import Path
+import numpy
+
+def compileShader(shaderType, src):
+    id = glCreateShader(shaderType)
+    glShaderSource(id, src)
+    glCompileShader(id)
+
+    return id
+
+def createShader(vertex, fragment):
+    vs = compileShader(GL_VERTEX_SHADER, vertex)
+    fs = compileShader(GL_FRAGMENT_SHADER, fragment)
+
+    program = glCreateProgram()
+    glAttachShader(program, vs)
+    glAttachShader(program, fs)
+    glLinkProgram(program)
+    glValidateProgram(program)
+
+    glDeleteShader(vs)
+    glDeleteShader(fs)
+
+    return program
 
 def main():
     if not glfw.init():
@@ -12,12 +36,33 @@ def main():
 
     glfw.make_context_current(window)
 
+    vertices = [
+        -1.0, -1.0,
+        1.0, -1.0,
+        1.0, 1.0,
+
+        1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, -1.0
+    ]
+    verticesArray = numpy.array(vertices, dtype=numpy.float32)
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, verticesArray, GL_STATIC_DRAW)
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * 4, None)
+    glEnableVertexAttribArray(0)
+
+    vertex = open("vertex.shader").read()
+    fragment = open("fragment.shader").read()
+
+    shader = createShader(vertex, fragment)
+    glUseProgram(shader)
+
+    glUniform4f(glGetUniformLocation(shader, "theColor"), 0.2, 0.4, 0.7, 0.9)
+
     while not glfw.window_should_close(window):
-        glBegin(GL_TRIANGLES)
-        glVertex2f(0, 1)
-        glVertex2f(1, -1)
-        glVertex2f(-1, -1)
-        glEnd()
+        glDrawArrays(GL_TRIANGLES, 0, 6)
 
         glfw.swap_buffers(window)
         glfw.poll_events()
